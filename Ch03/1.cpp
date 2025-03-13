7eed9127aab59accbc9a537c1c44dd8b5209457c
hoastust2k7 <5b3g0036@stust.edu.tw>
2025-03-13 16:58:56 +0800

    add today file

---
+++ Ch03/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    printf("The size of char is: %d\n", sizeof(char));
+    printf("The size of short int is: %d\n", sizeof(short));
+    printf("The size of int is: %d\n", sizeof(int));
+    printf("The size of long is: %d\n", sizeof(long));
+    printf("The size of unsigned char is: %d\n", sizeof(unsigned char));
+    printf("The size of unsigned short is: %d\n", sizeof(unsigned short));
+    printf("The size of unsigned int is: %d\n", sizeof(unsigned int));
+    printf("The size of unsigned long int is: %d\n", sizeof(unsigned long int));
+    printf("The size of float is: %d\n", sizeof(float));
+    printf("The size of double is: %d\n", sizeof(double));
+    printf("The size of long double is: %d\n", sizeof(long double));
+}
---
+++ Ch03/2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    printf("A\nB\nC\nB\nE\nF\nG\nH\nI\nJ\nK\nL\nM\nN\nO\nP\nQ\nR\nS\nT\nU\nV\nW\nX\nY\nZ\n");
+}
