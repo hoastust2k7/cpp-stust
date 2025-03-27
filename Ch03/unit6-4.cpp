4b8cf3dab8d5748e5acf730dc6bd20086ad67ff9
hoastust2k7 <5b3g0036@stust.edu.tw>
2025-03-27 17:47:27 +0800

    aa

--- Ch03/unit6-4.cpp
+++ Ch03/unit6-4.cpp
@@ -5,14 +5,21 @@ int main(int argc, char *argv[]) {
     int a = 10;
     int b = 0;
     printf("假設執行前 a 的初始值為 10：\n結果\n");
+    printf("--------\n");
+    printf("||\n");
+    printf("|b = ++a|\n");
     b = ++a;
     printf("|a = %d, b = %d|\n", b, b );
+    printf("|b = a--|\n");
     b = a--;
     printf("|a = %d, b = %d|\n", b, a);
+    printf("|b = --a|\n");
     b = --a;
     printf("|a = %d, b = %d|\n", a, a);
+    printf("|b = a++|\n");
     b = a++;
     printf("|a = %d, b = %d|\n", b, a);
+    printf("--------\n");
     system("PAUSE");
     return 0;
 }
