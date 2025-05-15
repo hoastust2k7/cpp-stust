46626ba66e231fb209fff0d1e19dfa85ab98bbfa
hoastust2k7 <5b3g0036@stust.edu.tw>
2025-05-15 16:15:11 +0800

    test

---
+++ c-cpp0515/1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+
+int main(int argc, char *argv[]) {
+    printf("班級：五專資工一甲\n");
+    printf("學號：5B3G0036\n");
+    printf("姓名：陳相樺\n\n");
+
+    return 0;
+}
